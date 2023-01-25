93e0b1e02d2a67f24efe1d1abb66fc23be3f2395
ElRecipiente <nicolas.droux.2b@gmail.com>
2023-01-25 13:59:01 +0100

    add some js and fix flex in watches div

--- script.js
+++ script.js
@@ -17,8 +17,25 @@ const availableProducts = [
         description: "This is the third product",
         price: 15
     }
-]
+];
+
+// Une fonction displayAvailableProducts() qui est appelée une fois au chargement de la page, qui remplit la section avec les différents articles dispo dans notre variable availableProducts .
+// La méthode .push() des Array permet d’ajouter un élément à la fin du tableau.
+function displayAvailableProducts() {
+
+};
+
 // cart est un tableau dynamique qui contient les éléments que l'on veut acheter
 // L'ensemble de ces produits seront affiché dans le cart
 let cart = [];
+
+// addProductToCart() qui ajoute un article dans le tableau cart. ⚠️ Cette fonction ne touche pas du tout à l’affichage de votre page HTML, elle ne devrait faire que 2 lignes
+function addProductToCart() {
+
+};
+
+// displayCart() qui affiche le contenu du panier dans le aside (sous forme de liste).
+function displayCart() {
+
+};
 
