a02ffcb32d06ae7f70bf86c05bc66de7111c609c
Leah <leah.jl@proton.me>
2023-01-26 15:54:07 +0100

    update script

--- script.js
+++ script.js
@@ -101,7 +101,7 @@ function indexInCart(watch) {
 
 function addProductToCart(avPIndex) {
     let watch = availableProducts[avPIndex];
-    let index = indexInCart(watch)
+    let index = indexInCart(watch);
     if (index != -1) {
         cart[index].number += 1;
     }
@@ -113,15 +113,8 @@ function addProductToCart(avPIndex) {
 
 }
 
-function removeWatchFromCart(thisWatch) {
-    cart[thisWatch].number = 1;
-    cart.splice(thisWatch, 1)
-    displayCart();
-
-    console.log(cart);
-}
-
 displayAvailableProducts()
+
 
 // displayCart() qui affiche le contenu du panier dans le aside (sous forme de liste).
 const ulCart = document.querySelector(".grande ul");
@@ -145,10 +138,16 @@ function displayCart() {
         </select>
     </span>
     <div>
-        <button class="btn" onclick="removeWatchFromCart(${i})">
+        <button class="btn" onclick="delete()">
             <img src="images/delete.png" alt="icone">
         </button>
     </div>`;
         ulCart.append(watchInCart);
     }
 };
+
+function deleteWatch() {
+    cart.splice(i, 1)
+
+    displayCart();
+}
